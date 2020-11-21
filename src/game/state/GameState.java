0a272dd252a6a1dcf49ebe19b4125271e9072071
Beatzoid <epiccoder7@gmail.com>
2020-11-20 21:11:56 -0800

    ✨ Victory and Defeat
    Add win conditions and lose conditions and display a text to the screen.

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -2,6 +2,7 @@ package game.state;
 
 import controller.NPCController;
 import controller.PlayerController;
+import core.Condition;
 import core.Size;
 import entity.NPC;
 import entity.Player;
@@ -13,8 +14,18 @@ import game.ui.UIGameTime;
 import game.ui.UISicknessStats;
 import input.Input;
 import map.GameMap;
+import ui.Alignment;
+import ui.UIText;
+import ui.VerticalContainer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameState extends State {
+
+    private List<Condition> victoryConditions;
+    private List<Condition> defeatConditions;
+    private boolean playing;
 
     /**
      * The GameState handles the game state
@@ -27,8 +38,16 @@ public class GameState extends State {
     public GameState(Size windowSize, Input input) {
         super(windowSize, input);
         gameMap = new GameMap(new Size(20, 20), spriteLibrary);
+        playing = true;
         initializeCharacters();
         initializeUI(windowSize);
+        initializeConditions();
+    }
+
+    private void initializeConditions() {
+        victoryConditions = List.of(() -> getNumberOfSick() == 0);
+        // If the number of sick NPC's is greater than 25% of the total number of NPC's, the player loses
+        defeatConditions = List.of(() -> (float) getNumberOfSick() / getNumberOfNPCs() > 0.25);
     }
 
     private void initializeUI(Size windowSize) {
@@ -44,6 +63,8 @@ public class GameState extends State {
         gameObjects.add(circle);
 
         initializeNPCs(200);
+        // 0 = Instant win
+        // 25% or more of the total number of NPC's = Instant lose
         makeNumberOfNPCsSick(10);
     }
 
@@ -62,12 +83,44 @@ public class GameState extends State {
         }
     }
 
+    @Override
+    public void update() {
+        super.update();
+
+        if (playing) {
+            if (victoryConditions.stream().allMatch(Condition::isMet)) {
+                win();
+            }
+            if (defeatConditions.stream().allMatch(Condition::isMet)) {
+                lose();
+            }
+        }
+    }
+
+    private void lose() {
+        playing = false;
+
+        VerticalContainer loseContainer = new VerticalContainer(camera.getSize());
+        loseContainer.setAlignment(new Alignment(Alignment.Position.CENTER, Alignment.Position.CENTER));
+        loseContainer.addUIComponent(new UIText("DEFEAT!"));
+        uiContainers.add(loseContainer);
+    }
+
+    private void win() {
+        playing = false;
+
+        VerticalContainer winContainer = new VerticalContainer(camera.getSize());
+        winContainer.setAlignment(new Alignment(Alignment.Position.CENTER, Alignment.Position.CENTER));
+        winContainer.addUIComponent(new UIText("VICTORY!"));
+        uiContainers.add(winContainer);
+    }
+
     /**
      * Get the number of sick people
      */
     public long getNumberOfSick() {
-        return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> humanoid.isAffectedBy(Sick.class) && !humanoid.isAffectedBy(Isolated.class))
+        return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> npc.isAffectedBy(Sick.class) && !npc.isAffectedBy(Isolated.class))
                 .count();
     }
 
@@ -75,8 +128,8 @@ public class GameState extends State {
      * Get the number of isolated people
      */
     public long getNumberOfIsolated() {
-        return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> humanoid.isAffectedBy(Sick.class) && humanoid.isAffectedBy(Isolated.class))
+        return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> npc.isAffectedBy(Sick.class) && npc.isAffectedBy(Isolated.class))
                 .count();
     }
 
@@ -84,8 +137,15 @@ public class GameState extends State {
      * Get the number of healthy people
      */
     public long getNumberOfHealthy() {
-        return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> !humanoid.isAffectedBy(Sick.class))
+        return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> !npc.isAffectedBy(Sick.class))
                 .count();
+    }
+
+    /**
+     * Get the number of NPC's
+     */
+    public long getNumberOfNPCs() {
+        return getGameObjectsOfClass(NPC.class).size();
     }
 }
